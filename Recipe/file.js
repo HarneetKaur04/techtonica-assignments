9eb9a8280c1ec68d9ff5ce29a7e5d6022b7831bc
HarneetKaur04 <harneetsaini40@gmail.com>
2022-08-03 08:14:30 -0700

    Recipe File Updated with layout

---
+++ Recipe/file.js
@@ -0,0 +1,59 @@
+let like_button = document.createElement("btn");
+like_button.innerHTML = "Like"
+like_button.style.background = "gray"
+like_button.style.display= "block"
+like_button.style.width= "10%"
+like_button.style.border= "solid blue"
+like_button.style.transition = "width"
+
+document.querySelector(".header").appendChild(like_button)
+like_button.onclick = function (){
+  alert("Thank you for liking my recipe!")}
+
+
+
+let secret_ingredient = document.createElement('button');
+secret_ingredient.innerHTML = "Click to know my secret ingredient"
+let secret_ingredient_stored = document.querySelector("ul")
+secret_ingredient_stored.appendChild(secret_ingredient)
+
+let new_li = document.createElement('li');
+new_li.innerHTML = "Maple Syrup!!!"
+new_li.style.display = "none"
+
+secret_ingredient_stored.appendChild(new_li)
+
+// secret_ingredient.addEventListener("click", function() {
+//   if (new_li.style.display === "none") {
+//       new_li.style.display = "block";
+//     } else {
+//       new_li.style.display = "none";
+//     }
+// }
+// )
+
+secret_ingredient.addEventListener("click", function() {
+  if (new_li.style.display === "none") {
+      new_li.style.display = "block";
+    } else {
+      new_li.style.display = "none";
+    }
+}
+)
+
+
+// function show() {
+//   let divide = document.getElementById("info");
+//   if (divide.style.display === "none") {
+//     divide.style.display = "block";
+//   } else {
+//     divide.style.display = "none";
+//   }
+// // }
+
+function haha() {
+ return "haha"
+}
+
+let lol = "lol"
+
