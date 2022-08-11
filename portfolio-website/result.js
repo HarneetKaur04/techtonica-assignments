c20e8b243bb1179e65abf41591e8530990a97560
HarneetKaur04 <harneetsaini40@gmail.com>
2022-08-11 08:39:01 -0700

    Changed Form to include phone number and required fields

--- portfolio-website/result.js
+++ portfolio-website/result.js
@@ -1,4 +1,4 @@
 const urlParams = new URLSearchParams(window.location.search);
 
 // Get the value of "some_key" in eg "?firstname=aaa&lastname=aaa&lastname=aaa&subject=aaaaaaaa"
-document.getElementById("result").innerHTML = "firstname: " + urlParams.get('firstname') + " lastname: " + urlParams.get('lastname') + " email: " + urlParams.get("email") + "<br>" + " Message: " + urlParams.get("subject")
+document.getElementById("result").innerHTML = "Name: " + urlParams.get('fullname') + " Phone: " + urlParams.get('phonenumber') + " Email: " + urlParams.get("email") + "<br>" + " Message: " + urlParams.get("subject")
